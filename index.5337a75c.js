327bb854f91d98143aee989b0ca8ae061c4ceb41
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-11-14 05:12:52 +0000

    Deploying to gh-pages from @ AndriiTkac4ov/goit-js-hw-10@90e23cafe1df789c5815928a1a3128afddbe0011 🚀

--- index.5337a75c.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/all?fields=name.official,capital,population,flags.svg,languages").then((l=>l.json())).then((l=>{console.log(l)})).catch((l=>{console.log(l)}));
-//# sourceMappingURL=index.5337a75c.js.map
